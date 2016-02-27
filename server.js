c674808443c1ce4cabf20522f599a6a5da2d7443
Britton Walker <bwalker1801@gmail.com>
2016-02-27 19:05:24 -0300

    get, post, and delete routes for todos api

--- server.js
+++ server.js
@@ -10,12 +10,62 @@ var methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost/bboystodo')
 
-app.use(express.static(_dirname + '/public'));
+// app.use(express.static(_dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
+
+
+// mongoose model ================
+
+var Todo = mongoose.model('Todo', {
+  text : String
+});
+
+// routes ==============
+
+app.get('/api/todos', function(req, res) {
+  Todo.find(function(err, todos) {
+    if (err)
+      res.send(err)
+
+    res.json(todos);
+  });
+});
+
+app.post('/api/todos', function(req, res) {
+  Todo.create({
+    text : req.body.text,
+    done : false
+  }, function(err, todo) {
+    if (err)
+      res.send(err);
+
+      Todo.find(function(err, todos) {
+          if (err)
+            res.send(err)
+          res.json(todos);
+      });
+  });
+});
+
+app.delete('/api/todos/:todo_id', function(req, res) {
+  Todo.remove({
+    _id : req.params.todo_id
+  }, function(err, todo) {
+    if (err)
+      res.send(err)
+
+    Todo.find(function(err, todos) {
+      if (err)
+        res.send(err)
+      res.json(todos);
+    });
+  });
+});
+
 
 app.listen(8080);
 console.log('Todo app listening on port 8080');
