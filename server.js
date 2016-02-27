c19a85f2fcdcb5092aa4d41e43a902010c6b3425
Britton Walker <bwalker1801@gmail.com>
2016-02-27 18:48:10 -0300

    make server.js file and set up server

---
+++ server.js
@@ -0,0 +1,21 @@
+// set up ======
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+// configuration ===
+
+mongoose.connect('mongodb://localhost/bboystodo')
+
+app.use(express.static(_dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride());
+
+app.listen(8080);
+console.log('Todo app listening on port 8080');
