de0883c182a95a526d83d6d7cc73029d6b58d8d8
Britton Walker <bwalker1801@gmail.com>
2016-02-29 13:45:36 -0300

    move routes to separate folder and require in server.js

--- server.js
+++ server.js
@@ -17,52 +17,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
-// routes ==============
+// load routes
 
-app.get('/api/todos', function(req, res) {
-  Todo.find(function(err, todos) {
-    if (err)
-      res.send(err)
-
-    res.json(todos);
-  });
-});
-
-app.post('/api/todos', function(req, res) {
-  Todo.create({
-    text : req.body.text,
-    done : false
-  }, function(err, todo) {
-    if (err)
-      res.send(err);
-
-      Todo.find(function(err, todos) {
-          if (err)
-            res.send(err)
-          res.json(todos);
-      });
-  });
-});
-
-app.delete('/api/todos/:todo_id', function(req, res) {
-  Todo.remove({
-    _id : req.params.todo_id
-  }, function(err, todo) {
-    if (err)
-      res.send(err)
-
-    Todo.find(function(err, todos) {
-      if (err)
-        res.send(err)
-      res.json(todos);
-    });
-  });
-});
-
-app.get('*', function(req, res) {
-  res.sendfile('./public/index.html');
-})
-
+require('./app/routes')(app);
 
 app.listen(8080);
 console.log('Todo app listening on port 8080');
