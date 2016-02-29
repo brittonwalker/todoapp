49614772e87358cb9a82a18feeaf1c5787356344
Britton Walker <bwalker1801@gmail.com>
2016-02-29 13:52:26 -0300

    clean up server.js file

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var database = require('./config/database')
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 // configuration ===
-var database = require('./config/database')
 mongoose.connect(database.url)
 
 app.use(express.static(__dirname + '/public'));
@@ -18,8 +18,8 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
 // load routes
-
 require('./routes/routes')(app);
 
+// listen 
 app.listen(8080);
 console.log('Todo app listening on port 8080');
